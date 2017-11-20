a04ccd553e84ac43dcc0065736a9a243546e63e9
James <james.hong@cs.stanford.edu>
2017-11-19 19:45:07 -0800

    Add response delay to unique IPs experiment

--- experiments/unique-ips/server.py
+++ experiments/unique-ips/server.py
@@ -2,23 +2,29 @@
 """A server that echos the client's IP address"""
 
 import argparse
+import time
 
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
 
 DEFAULT_PORT = 8080
+DEFAULT_DELAY = 1
 
 
-class EchoIpHandler(BaseHTTPRequestHandler):
+def build_handler(delay):
+    class EchoIpHandler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        ip, port = self.client_address
-        responseData = '%s:%d' % (ip, port)
-        self.send_response(200)
-        self.send_header('Content-Length', len(responseData))
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(responseData)
+        def do_GET(self):
+            ip, port = self.client_address
+            responseData = '%s:%d' % (ip, port)
+            time.sleep(delay)
+            self.send_response(200)
+            self.send_header('Content-Length', len(responseData))
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(responseData)
+
+    return EchoIpHandler
 
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
@@ -28,11 +34,14 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', '-p', type=int, default=DEFAULT_PORT)
+    parser.add_argument('--delay', '-d', type=int, default=DEFAULT_DELAY,
+                        help='Number of seconds to delay before responding')
     return parser.parse_args()
 
 
 def main(args):
-    server = ThreadedHTTPServer(('', args.port), EchoIpHandler)
+    handler =build_handler(args.delay)
+    server = ThreadedHTTPServer(('', args.port), handler)
     print 'Serving on port', args.port
     server.serve_forever()
 
